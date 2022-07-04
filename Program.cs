555a19c333f91742e7288223efa65011fd2599e4
Nikita <nikita.bykov485@yandex.com>
2022-07-04 11:13:09 +0500

    First

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трехзначное число.");
+    
+    int num = Convert.ToInt32(Console.ReadLine());
+    int secondNumber = ((num/10)%10);
+
+    Console.WriteLine(secondNumber);
